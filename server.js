f99f353c53d0a784b56992e22b9cf1d6b773e02b
bnruchika <bnruchika2011@gmail.com>
2017-08-15 11:38:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/atricle-one', function (req, res) {
-  res.send('article one will be displayed here');
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
