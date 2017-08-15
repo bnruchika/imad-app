09c26dc5cc7c1985301b7273fedab0f95cf98923
bnruchika <bnruchika2011@gmail.com>
2017-08-15 11:07:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');//
-var morgan = require('morgan');//libruary
+var morgan = require('morgan');//librury
 var path = require('path');
 
 var app = express();
@@ -9,21 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req, res){
-res.send('Article one requested and will be served here');
-});
-
-app.get('/article-two', function(req, res){
-res.send('Article two requested and will be served here');
-});
-
-app.get('/article-three', function(req, res){
-res.send('Article three requested and will be served here');
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});                                                         //url requests
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
